46cd9fe7bb1b8717b6a3e50f7fe1df363ebf7ba4
(Student  C00306813) Conor Minihn <C00306813@setu.ie>
2025-11-12 14:32:59 +0000

    refactor: changed some variable names for clarity

--- 16_SFML_Games/minesweeper.cpp
+++ 16_SFML_Games/minesweeper.cpp
@@ -8,23 +8,23 @@ int minesweeper()
 
     RenderWindow app(VideoMode(400, 400), "Minesweeper!");
 
-    int w=32;
-    int grid[12][12];
-    int sgrid[12][12]; //for showing
+    int w=32; // width of tile
+    int grid[12][12]; // bombs and numbers
+    int sgrid[12][12]; // covered tiles
 
     Texture t;
     t.loadFromFile("images/minesweeper/tiles.jpg");
     Sprite s(t);
 
-    for (int i=1;i<=10;i++)
+    for (int i=1;i<=10;i++) // 10 = covered tile
      for (int j=1;j<=10;j++)
       {
         sgrid[i][j]=10;
-        if (rand()%5==0)  grid[i][j]=9;
+        if (rand()%5==0)  grid[i][j]=9; // 9 = bomb
         else grid[i][j]=0;
       }
 
-    for (int i=1;i<=10;i++)
+    for (int i=1;i<=10;i++) // checks for adjecant bombs
      for (int j=1;j<=10;j++)
       {
         int n=0;
@@ -53,8 +53,8 @@ int minesweeper()
                 app.close();
 
             if (e.type == Event::MouseButtonPressed)
-              if (e.key.code == Mouse::Left) sgrid[x][y]=grid[x][y];
-              else if (e.key.code == Mouse::Right) sgrid[x][y]=11;
+              if (e.key.code == Mouse::Left) sgrid[x][y]=grid[x][y]; // reveals square
+              else if (e.key.code == Mouse::Right) sgrid[x][y]=11; // places flag
         }
 
         app.clear(Color::White);
@@ -62,8 +62,8 @@ int minesweeper()
         for (int i=1;i<=10;i++)
          for (int j=1;j<=10;j++)
           {
-           if (sgrid[x][y]==9) sgrid[i][j]=grid[i][j];
-           s.setTextureRect(IntRect(sgrid[i][j]*w,0,w,w));
+           if (sgrid[x][y]==9) sgrid[i][j]=grid[i][j]; // uncovers board if bomb is selected
+           s.setTextureRect(IntRect(sgrid[i][j]*w,0,w,w));// otherwise fills board normally
            s.setPosition(i*w, j*w);
            app.draw(s);
           }
